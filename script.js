e4e7483106757ef1055aeb9e33ef770fd2b05470
Anna Isumonah <maisumonah@gmail.com>
2023-11-15 21:27:46 +0100

    improved responsiveness

--- script.js
+++ script.js
@@ -110,8 +110,6 @@ form.addEventListener('submit', (e) => {
         }
       } else {
         display.innerHTML = `<p class="initial-display" style="font-weight: bold; color:black"> No Item Found </p>`;
-        // initialDisplay.style.fontWeight = 'bold';
-        // initialDisplay.style.color = 'black';
       }
     })
     .catch((error) => {
@@ -124,14 +122,18 @@ form.addEventListener('submit', (e) => {
 
 function addBtn(data) {
   addedMovies.push(data);
+  console.log(addedMovies);
   sessionStorage.setItem('addedMovies', JSON.stringify(addedMovies));
 
   const button = document.querySelectorAll('.add-button');
 
   button.forEach((item) => {
     const plusIcon = item.querySelector('.plus-icon');
+    console.log(plusIcon);
     const minusIcon = item.querySelector('.minus-icon');
     plusIcon.addEventListener('click', (e) => {
+      console.log(plusIcon);
+      console.log(e);
       plusIcon.classList.add('hidden-icon');
       plusIcon.classList.remove('show-icon');
       minusIcon.classList.remove('hidden-icon');
@@ -181,38 +183,70 @@ function showMoviesOnSearchPage(eachMovie, display) {
 
 function showMoviesOnPage(eachMovie, item) {
   const stringMovie = JSON.stringify(eachMovie);
-  const eachMovieItem = document.createElement('li');
+  const eachMovieItem = document.createElement('div');
   eachMovieItem.classList.add('movie');
   eachMovieItem.innerHTML = `
-        <img class="movie-img" src=${eachMovie.Poster} />
-        <div class="movie-info">
-            <div class="title-rating">
-                <p class="movie-title">${eachMovie.Title}</p>
-                <p class="movie-rating"><i class="fa fa-star fa-stack"></i>${
-                  eachMovie.imdbRating
-                }
+        <div class="sm:hidden title-rating">
+          <p class="movie-title font-bold text-center text-[18px] sm:text-[20px] lg:text-[22px] xl:text-[24px] w-10/12 mx-auto">${eachMovie.Title} 
+          </p>
+        </div>
+
+        <div class="sm:flex">
+          <div class="movie-img-div mt-6 sm:mt-0 w-[80%] sm:h-[80%] sm:w-auto object-cover rounded mx-auto flex">
+            <img class="movie-img w-[100%] h-[100%] border-2 rounded" src=${eachMovie.Poster} />
+        </div>
+
+        <div class="movie-info flex flex-col justify-between p-[5px] ml-[21px] text-[14px] sm:text-[16px] lg:text-[18px] xl:text-[22px] my-4 sm:my-0 sm:justify-center">
+
+          <div class="hidden sm:block">
+            <div class="flex justify-evenly sm:w-[70%] md:w-[55%] items-start space-x-4">
+                <p class="movie-title font-bold text-[16px] sm:text-[18px] lg:text-[20px] xl:text-[24px] w-full">${eachMovie.Title}
+                </p>
+                <p class="movie-rating text-[#111827] font-bolder flex items-center sm:text-[14px] lg:text-[16px] xl:text-[18px]">
+                  <i class="fa fa-star fa-stack text-[#fec654] w-[15px] pr-[25px]"></i>${eachMovie.imdbRating}
                 </p>
             </div>
-            <div class="time-genre-watchlist">
-                <p class="runtime"> ${eachMovie.Runtime}</p>
-                <p class="genre"> ${eachMovie.Genre}</p>
-                <div class="movie-watchlist">
-                    <button class="add-button" >
-                        <i class="fa-solid fa-plus plus-icon" data-id=${
-                          eachMovie.imdbID
-                        }></i>
-                        <i class="fa-solid fa-minus minus-icon hidden-icon" data-id=${
-                          eachMovie.imdbID
-                        }></i>
-                    </button>
-                    <p>${eachMovie.watchList ? 'Remove' : 'WatchList'}</p>
-                </div>
+          </div>
+
+          <div class="time-genre-watchlist flex justify-between w-11/12 sm:w-full md:w-10/12 lg:w-[70%] text-[12px] sm:text-[14px] lg:text-[16px] xl:text-[18px] text-[#111827] sm:mt-2 items-start leading-[25px]">
+            <p class="runtime mr-2"> ${eachMovie.Runtime}</p>
+            <p class="genre mr-2"> ${eachMovie.Genre}</p>
+
+            <div class="hidden sm:block">
+              <button class="movie-watchlist flex justify-between add-button">
+
+                <p class="plus-icon" data-id=${eachMovie.imdbID}>
+                <i class="fa-solid fa-plus p-[2px] rounded-full bg-[#111827] text-white"></i>
+                Watchlist
+                </p>
+
+                <p class="minus-icon hidden-icon" data-id=${eachMovie.imdbID}>
+                <i class="fa-solid fa-minus p-[2px] rounded-full bg-[#111827] text-white"></i>
+                Remove
+                </p>
+
+              </button>
             </div>
-            <div class="movie-plot">
-                <p>${eachMovie.Plot}</p>
+
+            <div class="movie-watchlist flex justify-between text-[12px] items-start">
+              <p class="movie-rating text-[#111827] font-bolder flex items-start sm:hidden">
+                <i class="fa fa-star fa-stack text-[#fec654] w-[15px] pr-[18px]"></i>
+                ${eachMovie.imdbRating}
+              </p>
             </div>
+          </div>
+
+          <div class="movie-plot mt-4 sm:mt-2 w-10/12 sm:w-full md:w-10/12 text-[#6B7280]">
+            <p> A recently divorced man meets an emotionally devastated widow and they begin a love affair.
+            </p>      
+          </div>
+          <div class="add-button mt-4 w-8/12 mx-auto rounded sm:hidden text-[13px] " data=${eachMovie.imdbID}>
+            <button class="plus-icon bg-black text-white p-2 w-full" data-id=${eachMovie.imdbID}>Add to Watchlist</button>
+            <button class="minus-icon hidden-icon text-black border-2 border-black p-2" data-id=${eachMovie.imdbID}>Remove from Watchlist</button>
+          </div>
         </div>
-        `;
+      </div>
+    `;
 
   item.appendChild(eachMovieItem);
 }
--- scriptNew.js
+++ scriptNew.js
@@ -12,21 +12,45 @@ function showMoviesOnPage(eachMovie) {
   const eachMovieItem = document.createElement('li');
   eachMovieItem.classList.add('movie');
   eachMovieItem.innerHTML = `
-        <img class="movie-img" src=${eachMovie.Poster} />
-        <div class="movie-info">
-            <div class="title-rating">
-                <p class="movie-title">${eachMovie.Title}</p>
-                <p class="movie-rating"><i class="fa fa-star fa-stack"></i>${eachMovie.imdbRating}
+         <div class="sm:hidden title-rating">
+          <p class="movie-title font-bold text-center text-[18px] sm:text-[20px] lg:text-[22px] xl:text-[24px] whitespace-nowrap w-10/12 mx-auto">${eachMovie.Title} 
+          </p>
+        </div>
+
+        <div class="sm:flex">
+          <div class="movie-img-div mt-6 sm:mt-0 w-[80%] sm:h-[80%] sm:w-auto object-cover rounded mx-auto flex">
+            <img class="movie-img w-[100%] h-[100%] border-2 rounded" src=${eachMovie.Poster} />
+        </div>
+
+        <div class="movie-info flex flex-col justify-between p-[5px] ml-[21px] text-[14px] sm:text-[16px] lg:text-[18px] xl:text-[22px] my-4 sm:my-0 sm:justify-center">
+
+          <div class="hidden sm:block">
+            <div class="flex justify-evenly sm:w-[65%] md:w-[55%] items-start">
+                <p class="movie-title font-bold text-[16px] sm:text-[18px] lg:text-[20px] xl:text-[24px] w-full ">${eachMovie.Title}
+                </p>
+                <p class="movie-rating text-[#111827] font-bolder flex items-center sm:text-[14px] lg:text-[16px] xl:text-[18px]">
+                  <i class="fa fa-star fa-stack text-[#fec654] w-[15px] pr-[20px]"></i>${eachMovie.imdbRating}
                 </p>
             </div>
-            <div class="time-genre-watchlist">
-                <p class="runtime"> ${eachMovie.Runtime}</p>
-                <p class="genre"> ${eachMovie.Genre}</p>
+          </div>
+
+          <div class="time-genre-watchlist flex items-start justify-between w-10/12 sm:w-full md:w-10/12 lg:w-[70%] text-[12px] sm:text-[14px] lg:text-[16px] xl:text-[18px] text-[#111827] sm:mt-2">
+            <p class="runtime"> ${eachMovie.Runtime}</p>
+            <p class="genre"> ${eachMovie.Genre}</p>
+            <div class="movie-watchlist flex whitespace-nowrap justify-between">
+              <p class="movie-rating text-[#111827] font-bolder flex sm:hidden">
+                <i class="fa fa-star fa-stack  text-[#fec654] w-[15px] pr-[10px] leading-[20px]"></i>
+                ${eachMovie.imdbRating}
+              </p>
             </div>
-            <div class="movie-plot">
-                <p>${eachMovie.Plot}</p>
-            </div>
+          </div>
+
+          <div class="movie-plot mt-4 sm:mt-2 w-10/12 sm:w-full md:w-10/12 text-[#6B7280]">
+            <p> A recently divorced man meets an emotionally devastated widow and they begin a love affair.
+            </p>      
+          </div>
         </div>
+      </div>
         `;
 
   displayFavourite.appendChild(eachMovieItem);
